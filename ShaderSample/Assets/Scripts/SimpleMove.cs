9172d5da9caa25d84adb4386bc5fddfc379bccec
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-15 04:02:09 +0900

    Added Surface Scene11 Cutoff

---
+++ ShaderSample/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+  public Vector3 Distance = new Vector3(1, 0, 0);
+  public Vector3 Speed = new Vector3(1, 1, 1);
+  private Vector3 basePosition;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    this.basePosition = transform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.position = this.basePosition + new Vector3(
+      Mathf.Cos(Time.time * Speed.x) * Distance.x,
+      Mathf.Sin(Time.time * Speed.y) * Distance.y,
+      Mathf.Sin(Time.time * Speed.z) * Distance.z
+    );
+  }
+}
+
---
+++ ShaderSample/Assets/Shaders/Surface/CutoffCast.shader
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------------
+// 11_01 CutoffCast
+// デプスバッファのみに書き込む
+//-----------------------------------------------------------------------------
+Shader "Custom/Surface/CutoffCast"
+{
+	Properties
+	{
+	}
+
+	SubShader
+	{
+		Tags { "Queue" = "Geometry" }
+		LOD 200
+
+		Pass {
+			Zwrite On
+			ColorMask 0
+		}
+	}
+	FallBack "Diffuse"
+}
