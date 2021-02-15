66bde617ef2ff7e4c5bdd41e122eefda6a014c0a
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-15 10:51:03 +0900

    Refactor

--- ShaderSample/Assets/SceneAssets/Unlit/03_PulseNoise/PulseController.cs
+++ ShaderSample/Assets/SceneAssets/Unlit/03_PulseNoise/PulseController.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PulseController : MonoBehaviour
+namespace Unlit_Scene03_Pulse
 {
-  private Image image;
-
-  // Start is called before the first frame update
-  void Start()
+  public class PulseController : MonoBehaviour
   {
-    this.image = GetComponent<Image>();
-  }
+    private Image image;
 
-  // Update is called once per frame
-  void Update()
-  {
-    if(Input.GetMouseButton(0)) {
-      this.image.material.SetFloat("_Amount", Mathf.Sin(Time.time * 50f));
+    // Start is called before the first frame update
+    void Start()
+    {
+      this.image = GetComponent<Image>();
     }
 
-    if(Input.GetMouseButtonUp(0)) {
-      this.image.material.SetFloat("_Amount", 0);
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetMouseButton(0)) {
+        this.image.material.SetFloat("_Amount", Mathf.Sin(Time.time * 50f));
+      }
+
+      if (Input.GetMouseButtonUp(0)) {
+        this.image.material.SetFloat("_Amount", 0);
+      }
     }
   }
 }
