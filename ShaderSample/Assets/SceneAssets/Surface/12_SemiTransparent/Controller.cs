ac8318b2c51336dd970b99694f05298f340ba2c5
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-15 11:10:00 +0900

    Updated Surface 12 SemiTransparent

--- ShaderSample/Assets/SceneAssets/Surface/12_SemiTransparent/Controller.cs
+++ ShaderSample/Assets/SceneAssets/Surface/12_SemiTransparent/Controller.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+namespace Surface_Scene12_SemiTransparent
 {
+  public class Controller : MonoBehaviour
+  {
+    private Renderer render;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      this.render = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if (Input.GetMouseButton(0)) {
+        Color c = this.render.material.GetColor("_Color");
+        c.a = Mathf.Abs(Mathf.Sin(Time.time));
+        this.render.material.SetColor("_Color", c);
+      }
     }
+  }
 }
