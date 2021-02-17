562e5f0be1032add2a0681955112a7ece9c54dca
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-17 11:51:26 +0900

    Added PostEffect 03 Sepia

---
+++ ShaderSample/Assets/SceneAssets/PostEffect/03_Sepia/Sepia.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PostEffect_Scene03_Sepia
+{
+  public class Sepia : MonoBehaviour
+  {
+    public Material _Sepia;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+      Graphics.Blit(source, destination, _Sepia);
+    }
+  }
+}
