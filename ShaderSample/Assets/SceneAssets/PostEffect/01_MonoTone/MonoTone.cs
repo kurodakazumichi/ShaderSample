95009be9bff8336137563df24f8e5ac99259fab4
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-17 09:58:27 +0900

    Added PostEffect 01 MonoTone

---
+++ ShaderSample/Assets/SceneAssets/PostEffect/01_MonoTone/MonoTone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PostEffect_Scene01_MonoTone
+{
+  public class MonoTone : MonoBehaviour
+  {
+    public Material _MonoTone;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+      Graphics.Blit(source, destination, _MonoTone);
+    }
+  }
+}
+
